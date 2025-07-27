75281a994f083000f35b2d5a87ddbcc84e01da12
nmv47786 <nikhilmvishwanath@gmail.com>
2025-07-27 18:22:32 -0400

    prototype for red squiggles, but still functional

--- src/extension.ts
+++ src/extension.ts
@@ -2,48 +2,66 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let lastAlertedWords = new Set<string>();
+
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "red-squiggles" is now active!');
+	const diagnosticCollection = vscode.languages.createDiagnosticCollection('squiggles');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('red-squiggles.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code from Red Squiggles!');
-	});
+	if (vscode.window.activeTextEditor) {
+		updateDiagnostics(vscode.window.activeTextEditor.document, diagnosticCollection);
+	}
 
-
-	let alerting = vscode.commands.registerCommand('red-squiggles.alerting', () => {
-		vscode.window.showInformationMessage('Choose an option', 'A', 'B', 'C')
-		.then(selection => {
-			if (selection === 'A'){
-				for (let i = 0; i < 10; i ++) {
-					setTimeout(() => {
-						vscode.window.showErrorMessage(`ALERT ${i + 1}: You picked A`);
-					}, i * 500);
-				}
-			} else if (selection === 'B') {
-				vscode.window.showInputBox({ prompt: 'I am EV1L, Who are you' })
-				.then(output => {
-					if (output) {
-						vscode.window.showWarningMessage(`Hello ${output}`);
-					}
-				});
-			} else {
-				vscode.window.showInformationMessage('fine...');
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeTextDocument(e => {
+			updateDiagnostics(e.document, diagnosticCollection);
+		}),
+		vscode.window.onDidChangeActiveTextEditor(editor => {
+			if (editor) {
+				updateDiagnostics(editor.document, diagnosticCollection);
 			}
-		});
-	});
+		}), diagnosticCollection
+	);
+}
 
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(alerting);
+function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection) {
+	const diagnostics: vscode.Diagnostic[] = [];
+	const currentAlertedWords = new Set<string>();
+
+	for (let i = 0; i < document.lineCount; i++) {
+		const line = document.lineAt(i);
+		const words = line.text.match(/\S+/g);
+		if (!words) continue;
+
+		let startIndex = 0;
+		for (const word of words) {
+			const start = line.text.indexOf(word, startIndex);
+			if (start === -1) continue;
+
+			const range = new vscode.Range(i, start, i, start + word.length);
+			const diagnostic = new vscode.Diagnostic(
+				range,
+				`Error with ${word}`,
+				vscode.DiagnosticSeverity.Error
+			);
+			diagnostics.push(diagnostic);
+
+
+			if (!lastAlertedWords.has(word)) {
+				setTimeout(() => {
+					vscode.window.showErrorMessage(`Error: "${word}"`);
+				}, 0);
+			}
+
+			currentAlertedWords.add(word);
+			startIndex = start + word.length;
+		}
+	}
+
+	lastAlertedWords = currentAlertedWords;
+
+	collection.set(document.uri, diagnostics);
 }
 
 // This method is called when your extension is deactivated
